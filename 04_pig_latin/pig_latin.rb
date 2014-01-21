f7995ac1f7ba999da3e25f9b08ab45094e0999f6
Joshua Seaton <joshuaseaton@Joshua-Seatons-MacBook-Pro.local>
2014-01-21 15:40:49 -0500

    Test Complete

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,12 +1,55 @@
-	def translate(string)
-		vowels = ["a", "e", "i", "o", "u"]
-		if vowels.include?(string[0])
-		    string + "ay"
-		else 
-			if vowels.include?(string[1])
-				string[1..-1] + string[0] + "ay"
+def nocons(word)
+	word + "ay"
+end
+
+def onecons(word)
+	word[1..-1] + word[0] + "ay"
+end
+
+def twocons(word)
+	word[2..-1] + word[0..1] + "ay"
+end
+
+def threecons(word)
+	word[3..-1] + word[0..2] + "ay"
+end
+
+def qurules(word)
+	twocons(word)
+end
+
+def qucons(word)
+	threecons(word)
+end
+
+def piglatin(word)
+	vowels = ["a", "e", "i", "o", "u"]
+	
+		
+			if vowels.include?(word[0])
+		    	nocons(word)
+		    elsif word[0..1] == "qu"		    	
+		    	qurules(word)	
+		    elsif word[1..2] == "qu"
+		    	qucons(word)
 			else 
-				string [2..-1] + string[0..1] + "ay"
+		  		if vowels.include?(word[1])
+			    onecons(word)
+			else 
+				if vowels.include?(word[2])
+					twocons(word)
+				else 
+					threecons(word)
+				end			
 			end			
-		end
 	end
+end
+
+
+def translate(string)
+	words = string.split(" ")
+    words_array = words.map do |word|
+    	piglatin(word)
+    end
+	words_array.join(" ")
+end
